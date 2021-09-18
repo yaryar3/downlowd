f3ba5a113f4259b7502b06d210998491752d9902
Yar <inmag6@ya.ru>
2021-09-18 16:31:46 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function move() {
+    var elem = document.getElementById("greenBar");
+
+  
+   
+  var width = 10;
+  
+  
+    var id = setInterval(frame, 10);
+    function frame() {
+
+  
+    if (width >= 100) {
+
+  
+   
+  clearInterval(id);
+ 
+  
+   } else {   width++;
+
+  
+   elem.style.width = width + '%';  
+
+  
+       elem.innerHTML = width * 1  + '%';
+    }
+   }
+  }
+  
